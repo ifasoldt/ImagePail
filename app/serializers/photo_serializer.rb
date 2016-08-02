21811fc9f20ef613fe4696bcb089ef1300053749
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-02 17:25:05 -0400

    Adds extra image size of 100x100

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -1,8 +1,14 @@
 class PhotoSerializer < ActiveModel::Serializer
   attributes :id, :caption
   has_one :image
+  has_one :other_sizes
 
   def image
     Refile.attachment_url(object, :image)
+  end
+
+  def other_sizes
+    image_array = []
+    image_array << Refile.attachment_url(object, :image, :fill, 100, 100)
   end
 end
