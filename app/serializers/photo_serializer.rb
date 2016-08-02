1f8ec32aad4acc04ff8bf9eb66cc9f731b8822ea
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-02 17:30:42 -0400

    Adds 5 extra image sizes

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -10,5 +10,10 @@ class PhotoSerializer < ActiveModel::Serializer
   def other_sizes
     image_array = []
     image_array << Refile.attachment_url(object, :image, :fill, 100, 100)
+    image_array << Refile.attachment_url(object, :image, :fill, 250, 250)
+    image_array << Refile.attachment_url(object, :image, :fill, 500, 500)
+    image_array << Refile.attachment_url(object, :image, :fill, 1000, 1000)
+    image_array << Refile.attachment_url(object, :image, :limit, 50, 50)
+    image_array
   end
 end
