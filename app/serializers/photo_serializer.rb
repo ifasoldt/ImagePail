1b274f140f6a623e00de6773bf5f594630a92d72
Isaiah Fasoldt <ifasoldt@gmail.com>
2016-08-02 17:36:55 -0400

    Adds hash keys to extra images

--- app/serializers/photo_serializer.rb
+++ app/serializers/photo_serializer.rb
@@ -9,11 +9,11 @@ class PhotoSerializer < ActiveModel::Serializer
 
   def other_sizes
     image_array = []
-    image_array << Refile.attachment_url(object, :image, :fill, 100, 100)
-    image_array << Refile.attachment_url(object, :image, :fill, 250, 250)
-    image_array << Refile.attachment_url(object, :image, :fill, 500, 500)
-    image_array << Refile.attachment_url(object, :image, :fill, 1000, 1000)
-    image_array << Refile.attachment_url(object, :image, :limit, 50, 50)
+    image_array << {one_hundred: Refile.attachment_url(object, :image, :fit, 100, 100)}
+    image_array << {two_fifty: Refile.attachment_url(object, :image, :fit, 250, 250)}
+    image_array << {five_hundred: Refile.attachment_url(object, :image, :fit, 500, 500)}
+    image_array << {one_thousand: Refile.attachment_url(object, :image, :fit, 1000, 1000)}
+    image_array << {fifty_profile: Refile.attachment_url(object, :image, :limit, 50, 50)}
     image_array
   end
 end
